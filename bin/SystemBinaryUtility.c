f10bb1a7b5b58f2f60fe2eaada7b202878539f45
JackCraftsmanship <jack9876541@gmail.com>
2026-01-30 17:00:43 +0900

    File Separate : SystemBinaryUtility

---
+++ bin/SystemBinaryUtility.c
@@ -0,0 +1,128 @@
+#include "./SystemBinaryUtility.h"
+
+EFI_STATUS SBU_Readline(IN SBU *This, OUT CHAR16 *Buffer, IN UINTN BufferSize) {
+    EFI_STATUS Status;
+    EFI_INPUT_KEY CharTemp;
+    UINTN index;
+
+    //cursor control variable
+    UINTN Pos = 0;      //End of Line in Buffer
+    UINTN cursor = 0;   //Current Cursor Position
+    UINTN base_row = gST->ConOut->Mode->CursorRow;  //base Row Position
+    UINTN base_col = gST->ConOut->Mode->CursorColumn;   //base Column Position
+
+
+    while(1){
+        //using BootService, EventListener
+        gBS->WaitForEvent(1, &gST->ConIn->WaitForKey, &index);
+
+        //check ReadError
+        Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &CharTemp);
+        
+        //pass when Error
+        if(EFI_ERROR(Status)) continue;
+
+        //move Cursor L ro R
+        if(CharTemp.UnicodeChar == 0) {
+            if(CharTemp.ScanCode == SCAN_LEFT && cursor > 0) {
+                cursor--;
+                gST->ConOut->SetCursorPosition(gST->ConOut, base_col + cursor, base_row);
+            }
+            if(CharTemp.ScanCode == SCAN_RIGHT && cursor < Pos) {
+                cursor++;
+                gST->ConOut->SetCursorPosition(gST->ConOut, base_col + cursor, base_row);
+            }
+            continue;
+        }
+
+        //other exception code handle & normal typing
+        if(CharTemp.UnicodeChar == CHAR_CARRIAGE_RETURN) {
+            Buffer[Pos] = L'\0';
+            Print(L"\r\n");
+            return EFI_SUCCESS;
+        } else if (CharTemp.UnicodeChar == CHAR_BACKSPACE) {
+            if(cursor > 0 && Pos > 0) {
+                cursor--;
+                Pos--;
+                Print(L"\b \b");
+                if(cursor != Pos) {
+                    for(int i = cursor; i < Pos; i++) {
+                    Buffer[i] = Buffer[i + 1];
+                    Print(L"%c", Buffer[i]);
+                    }
+                    Print(L" ");
+                    gST->ConOut->SetCursorPosition(gST->ConOut, base_col + cursor, base_row);
+                }
+                Buffer[Pos] = L'\0';
+            }
+        } else if(CharTemp.UnicodeChar == 0x15) { //ctrl + U
+            while (Pos > 0) {
+                Pos--;
+                Print(L"\b \b");
+            }
+            cursor = 0;
+            Buffer[0] = L'\0';
+        } else if(Pos < BufferSize - 1) {
+            if(cursor != Pos) {
+                for(int i = Pos; i > cursor; i--) {
+                Buffer[i] = Buffer[i - 1];
+                }
+            }
+            Buffer[cursor++] = CharTemp.UnicodeChar;
+            Buffer[++Pos] = L'\0';
+            Print(L"%c", CharTemp.UnicodeChar);
+            if(cursor != Pos) {
+                for(int i = cursor; i < Pos; i++) {
+                Print(L"%c", Buffer[i]);
+                }
+                gST->ConOut->SetCursorPosition(gST->ConOut, base_col + cursor, base_row);
+            }
+        }
+
+    }
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS SBU_ReBoot(IN SBU *This, IN CHAR16 *Option) {
+    EFI_RESET_TYPE resetType;
+
+    if(!StrCmp(Option, L"ColdReset")) {
+        Print(L"Reset with device shutdown.");
+        resetType = EfiResetCold;
+    }
+    else if(!StrCmp(Option, L"NormalReset")) {
+        Print(L"Reset without device shutdown");
+        resetType = EfiResetWarm;
+    } else {
+        Print(L"Enter Correct ResetType : ColdReset or Reset\r\n");
+        return EFI_SUCCESS;
+    }
+    
+    gST->RuntimeServices->ResetSystem(resetType, EFI_SUCCESS, 0, NULL);
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS SBU_Shutdown(IN SBU *This) {
+    gST->RuntimeServices->ResetSystem(EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS SBU_OptionHandler(IN SBU *This, IN CHAR16 *SourceString, IN CHAR16 *OptionIdentifier) {
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS SBU_WhoamI(IN SBU *This) {
+    Print(L"This shell is part of Custom EFI Boot Service\r\nMade by Jack::ZeroCP\r\n");
+    return EFI_SUCCESS;
+}
+
+EFI_STATUS SBU_InitializeLib(IN SBU *This)
+{
+    This->ReadLine = SBU_Readline;
+    This->RebootCommand = SBU_ReBoot;
+    This->ShutdownCommand = SBU_Shutdown;
+    This->WhoamI = SBU_WhoamI;
+    This->OptionHandler = SBU_OptionHandler; 
+
+    return EFI_SUCCESS;
+}
---
+++ bin/SystemBinaryUtility.h
@@ -0,0 +1,87 @@
+#ifndef _ZCP_UEFI_SystemBinaryUtility_
+#define _ZCP_UEFI_SystemBinaryUtility_
+
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+/**This Object is renamed identifier of _System_Binary_Utility.
+ * It will give the interface of Basic Termianl Control in Boot Service
+ * @note when init, attach with this function : SBU_InitializeLib
+ */
+typedef struct _System_Binary_Utility SBU;
+
+struct _System_Binary_Utility {
+
+    /**This is Part of _System_Binary_Utility or SBU, 
+     * Read a Line and save into System Text Buffer.
+     * @param This self
+     * @param buffer System Text Buffer
+     * @param BufferSize Max Buffer Size of System
+     * @return When Error, return EFI_ERROR, when Normal, return EFI_SUCCESS
+     */
+    EFI_STATUS (*ReadLine)(
+        IN SBU *This,
+        OUT CHAR16 *Buffer,
+        IN UINTN BufferSize
+    );
+
+    /**This is Part of _System_Binary_Utility or SBU, 
+     * Call Reboot Command with Option.
+     * @note This will be change from select-one-at-a-time to Option parsing
+     * @param This self
+     * @param Option option of Reboot command. See EFI Spec docs
+     * @return When Error, return EFI_ERROR, when Normal, return EFI_SUCCESS
+     */
+    EFI_STATUS (*RebootCommand)(
+        IN SBU *This,
+        IN CHAR16 *Option
+    );
+
+    /**This is Part of _System_Binary_Utility or SBU, 
+     * Call shutdown Command.
+     * @note This will be add some Options
+     * @param This self
+     * @return When Error, return EFI_ERROR, when Normal, return EFI_SUCCESS
+     */
+    EFI_STATUS (*ShutdownCommand)(
+        IN SBU *This
+    );
+
+    /**This is Part of _System_Binary_Utility or SBU, 
+     * Internal Option Handler for handle Option in command.
+     * @note Currently not vaild
+     * @param This self
+     * @param SourceString Source string that want to parsing option
+     * @param OptionIdentifier Option Identifier
+     * @return When Error, return EFI_ERROR, when Normal, return EFI_SUCCESS
+     */
+    EFI_STATUS (*OptionHandler)(
+        IN SBU *This,
+        IN CHAR16 *SourceString,
+        IN CHAR16 *OptionIdentifier
+    );
+
+    /**This is Part of _System_Binary_Utility or SBU, 
+     * Who Am I String Printer. Suitable for testing BootService.
+     * @param This self
+     * @return When Error, return EFI_ERROR, when Normal, return EFI_SUCCESS
+     */
+    EFI_STATUS (*WhoamI)(
+        IN SBU *This
+    );
+};
+
+EFI_STATUS SBU_Readline(IN SBU *This, OUT CHAR16 *Buffer, IN UINTN BufferSize);
+
+EFI_STATUS SBU_ReBoot(IN SBU *This, IN CHAR16 *Option);
+
+EFI_STATUS SBU_Shutdown(IN SBU *This);
+
+EFI_STATUS SBU_OptionHandler(IN SBU *This, IN CHAR16 *SourceString, IN CHAR16 *OptionIdentifier);
+
+EFI_STATUS SBU_WhoamI(IN SBU *This);
+
+EFI_STATUS SBU_InitializeLib(IN SBU *This);
+
+#endif
